d57c229b9f09984c91360189e607ca94a2ffbed6
KatarzynaRomaniak <126468477+KatarzynaRomaniak@users.noreply.github.com>
2023-03-20 13:04:06 +0100

    Add files via upload

---
+++ lesson-03/methhhsss.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Insect {
+public:
+	string name;
+
+	Insect(string name){ this->name = name; }
+};
+
+class Flower {
+private:
+	string species;
+	string color;
+	vector<Insect> insects;
+public:
+	void Infestation(Insect i) {
+		insects.push_back(i);
+	}
+	void PrintInfestation() {
+		for (auto i : insects) {
+			cout << i.name << endl;
+		}
+	}
+};
+
+int main()
+{
+	Insect i("LeptontarsaDecemlineata");
+	Flower f;
+	f.Infestation(i);
+	f.PrintInfestation();
+}
