ea792a5e415e45eed7efeb24a35cc890c0615f20
KatarzynaRomaniak <126468477+KatarzynaRomaniak@users.noreply.github.com>
2023-03-27 12:57:36 +0200

    Add files via upload

---
+++ lesson-04/weweconstructors.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+class Employee {
+public:
+	Employee(int);
+	Employee(string);
+private:
+	string name;
+	int id;
+};
+
+Employee::Employee(int n){
+id = n;
+cout << id;
+}
+
+Employee::Employee(string name) {
+this->name = name;
+cout << name;
+}
+
+
+int main()
+{
+	Employee person1("Wlodzimierz\n");
+	Employee person2("Jesse\n");
+	Employee person3(3269);
+}
+
