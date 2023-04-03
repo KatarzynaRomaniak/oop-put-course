6fbb67008ee3a6dfc2f93257b9df4ea016cfb382
KatarzynaRomaniak <126468477+KatarzynaRomaniak@users.noreply.github.com>
2023-04-03 12:44:43 +0200

    Add files via upload

---
+++ lesson-05/interfaceandimmability.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+	virtual void WhichShape() = 0;
+};
+
+class Square:public Shape {
+	string shape = "square";
+
+public:
+	virtual void WhichShape(){
+		cout << "The shape is " <<shape<< endl;
+	}
+};
+
+
+int main()
+{
+    Square k;
+	k.WhichShape();
+}
+
